7a0f729d7b2d9e9d312251ce337a195145ae47a4
Atle Pedersen <atle.pedersen@sintef.no>
2022-04-19 15:16:44 +0200

    fikset parse_pdf_to text.py slik at den gjør riktig encoding

--- parse_pdf_to_text.py
+++ parse_pdf_to_text.py
@@ -1,6 +1,35 @@
-from tika import parser # pip install tika
-filename = 'energi21'
-raw = parser.from_file(filename + '.pdf')
-sourcefile = open(filename + '.txt','w', encoding="utf-8")
-print(raw['content'],file = sourcefile)
-sourcefile.close()
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import os
+from PyPDF2 import PdfFileReader
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument('input', help='Relative or absolute path of the input PDF file')
+
+    args = parser.parse_args()
+    pdfreader = PdfFileReader(args.input)
+    fname = os.path.splitext(os.path.basename(args.input))[0]
+    x = pdfreader.numPages
+    outputfile = open('{}.txt'.format(fname),'w',encoding="utf-8")
+    text = ""
+    for i in range (0,x):
+        pageobj = pdfreader.getPage(i)
+        text += pageobj.extractText()#.encode('latin-1')
+    print(text)
+    outputfile.write(text)
+    outputfile.close()
+
+if __name__ == '__main__':
+    main()
+# Denne trenger java installert så jeg dropper den
+#from tika import parser # pip install tika
+#filename = 'energi21'
+#raw = parser.from_file(filename + '.pdf')
+#sourcefile = open(filename + '.txt','w', encoding="utf-8")
+#print(raw['content'],file = sourcefile)
+#sourcefile.close()
